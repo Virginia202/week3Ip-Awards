dadf80b8c6d0700a1b91fb466a8d1d28e5b19379
Virginia202 <virgisonie20@gmail.com>
2020-11-28 13:27:59 +0300

    added the views

--- awarrds/views.py
+++ awarrds/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Creating views
+def index(request):
+    return HttpResponse('Welcome to the Award-site')
